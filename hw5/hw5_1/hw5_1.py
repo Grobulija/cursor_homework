19e90a598c38982843236498b22cf8f8145dba46
Grobulija <skleppic@gmail.com>
2021-05-17 04:53:59 -0700

    Added homework 5.1

---
+++ hw5/hw5_1/hw5_1.py
@@ -0,0 +1,18 @@
+import pytest
+from game import TicTacToe
+
+
+def setup_module(module):
+    global game
+    game = TicTacToe()
+    game.board[4] = "X"
+
+
+def test_available_moves():
+    assert (4 not in game.available_moves())
+
+
+def test_make_move():
+    assert not (game.make_move(4, "X"))
+    assert (game.make_move(5, "X"))
+    assert (game.board[5] != " ")
