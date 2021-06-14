cded58fa4c6c3057d116668d168cdea3c061f4b1
Grobulija <skleppic@gmail.com>
2021-06-14 15:45:02 +0300

    Added Homework 14

---
+++ hw14/tests/test_api.py
@@ -0,0 +1,54 @@
+from tests.conftest import client, todos
+from config import Config
+import json
+
+
+def test_create(client, todos):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = client.post("/todos", headers=headers, json=todos)
+    assert response.status_code == 200
+    assert response.json['1'] == "text"
+
+
+def test_list(client):
+    response = client.get('/todos')
+    assert response.status_code == 200
+    assert response.json['1'] == "text"
+
+
+def test_update(client):
+    update_data = {
+        "text": "blablabla"
+    }
+
+    response = client.put("/todos/1", json=update_data)
+    assert response.status_code == 200
+    get_response = client.get("/todos/1")
+    assert get_response.status_code == 200
+    assert get_response.json['1'] == "blablabla"
+
+
+def test_delete(client):
+    response = client.delete("/todos/1")
+    assert response.status_code == 204
+    response = client.get("/todos/1")
+    assert response.status_code == 404
+
+
+def test_weather(client):
+    Config.WEATHER_API_KEY = "776c974755msh3412d10ca84cccep13334ajsn4c81086b6ea1"
+    Config.WEATHER_API_URL = "https://community-open-weather-map.p.rapidapi.com/find"
+    Config.WEATHER_API_HOST = "community-open-weather-map.p.rapidapi.com"
+    response = client.get('/weather?city=Lviv')
+    assert response.status_code == 200
+    response = client.get('/weather?city=London,Lviv,Kiev')
+    assert response.status_code == 200
+    response = client.get('/weather?city=, ,')
+    assert response.status_code == 200
+    response = client.get('/weather?city=London, L')
+    assert response.status_code == 200
+    response = client.get('/weather?city=L')
+    assert response.status_code == 404
+
